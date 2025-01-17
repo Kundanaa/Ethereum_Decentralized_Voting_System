ab4783f120170faa8e0740a7fdcefc49ba36c861
Kundanaa <112239353+Kundanaa@users.noreply.github.com>
2025-01-17 12:27:42 +0000

    updated

---
+++ frontend/src/components/Home/Features.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Feature from "../Features/Feature";
+import { MdGppGood, MdLibraryAddCheck, MdLock, MdShare } from "react-icons/md";
+
+const Features = () => {
+  return (
+    <div className="features-wrapper">
+      <div className="title-large">Amazing Features</div>
+      <div className="title-small">
+        Ethereum Decentralized Voting Application using BlockChain
+      </div>
+
+      <div className="mobile-wrapper">
+        <div>
+          <Feature title="Immutability" icon={<MdLock />} align="right">
+            <p>
+              Immutability means something that can’t be changed or altered.
+              This is one of the top blockchain features that help to ensure
+              that the technology will remain as it is, a permanent, unalterable
+              network.
+            </p>
+          </Feature>
+        </div>
+
+        <div className="mobile-container">
+          <img src="/mobile.png" />
+        </div>
+
+        <div>
+          <Feature title="Enhanced Security" icon={<MdGppGood />} align="left">
+            <p>
+            Regular security audits, continuous monitoring, and adherence
+            to established standards also play pivotal roles in safeguarding 
+            the entire voting process against potential threats.
+            </p>
+          </Feature>
+        </div>
+        <div>
+          <Feature title="Decentralized" icon={<MdShare />} align="right">
+            <p>
+            Each transaction, representing a vote, is cryptographically secured and 
+            immutable, providing an auditable trail of the entire voting process 
+            while preserving voter anonymity.
+            </p>
+          </Feature>
+        </div>
+        <div>
+          <Feature
+            title="Distributed Ledger"
+            icon={<MdLibraryAddCheck />}
+            align="left"
+          >
+            <p>
+            Utilizing distributed ledger technology enhances transparency and immutability,
+            as every transaction is recorded across multiple nodes, making it exceedingly 
+            difficult to manipulate the data. 
+            </p>
+          </Feature>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
